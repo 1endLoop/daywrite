daff2aeb30ed57736817e80cf36166c5319a2517
co9mltjs <co9mltjs@gmail.com>
2025-06-10 15:55:36 +0900

    fix: inner tab 수정

--- front/src/pages/archive/history.style.js
+++ front/src/pages/archive/history.style.js
@@ -6,7 +6,6 @@ const S = {};
 S.TabMenu = styled.div`
   display: flex;
   width: 100%;
-  border-bottom: 1px solid #dcdcdc;
   a {
     text-decoration: none;
     width: 25%;
@@ -15,12 +14,15 @@ S.TabMenu = styled.div`
 
 S.TabItem = styled.div`
   width: 100%;
+  font-family: pretendard;
   text-align: center;
   padding: 0 0 16px 0;
   font-size: 16px;
   font-weight: 500;
+  font-weight: ${({ $isActive }) => ($isActive ? "700" : "500")};
   color: ${({ $isActive }) => ($isActive ? "#FF6F3F" : "#787878")};
-  border-bottom: ${({ $isActive }) => ($isActive ? "2px solid #000000" : "none")}; // ✅ 선택된 탭만 검정 보더
+  border-bottom: ${({ $isActive }) =>
+    $isActive ? "2px solid #282828" : "1px solid #dcdcdc"};
   cursor: pointer;
   position: relative;
 `;
